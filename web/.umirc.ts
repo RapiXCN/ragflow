5a2331e53871f885112fd1b7411ad3ce8db00a06
leo.yang <leo.yang@accenture.com>
2024-07-03 00:43:47 +0800

    logo change

--- web/.umirc.ts
+++ web/.umirc.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   esbuildMinifyIIFE: true,
   icons: {},
   hash: true,
-  favicons: ['/logo.svg'],
+  favicons: ['/AClogo.png'],
   clickToComponent: {},
   history: {
     type: 'browser',
